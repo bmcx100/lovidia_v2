aca68f81096dd2437c1231354b4e76078d2bb661
Ryan Hurst <bmcx100@gmail.com>
2024-03-28 21:55:51 -0400

    Prelaunch last and finally finally checkers

--- src/components/Helpers.tsx
+++ src/components/Helpers.tsx
@@ -2,21 +2,18 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import Breakpoints from "./tgg-utils/Breakpoints";
 
 const Helpers = () => {
   return (
     <div className="mb-12  min-w-[330px] bg-secondary pb-16 pt-5 md:pb-32 md:pt-12">
       <div className="mx-auto  max-w-[1024px] ">
         <div className="ml-3 text-brandLight xs:ml-8 sm:ml-16 md:ml-24">
-          <h2 className="m-0 p-0 font-normal xs:font-medium sm:mb-2 lg:text-4xl">
-            Our Helpers
-          </h2>
-          <p className="m-0 ml-4 p-0 lg:ml-8 lg:text-xl">
-            Click for information on:
-          </p>
+          <h2 className="m-0 p-0 font-normal xs:font-medium sm:mb-2 lg:text-4xl">Our Helpers</h2>
+          <p className="m-0 ml-4 p-0 lg:ml-8 lg:text-xl">Click for information on:</p>
         </div>
-        <div className="mx-4 mt-5 flex h-[150px] items-center justify-center gap-[10%] xs:mt-10 sm:mt-20 md:mt-32">
-          <div>
+        <div className=" mx-4 mt-5 flex h-full items-center justify-center gap-4">
+          {/* <div>
             <Image
               src="/images/helpers.png"
               alt="illustration of a man standing looking off into the distance"
@@ -24,17 +21,29 @@ const Helpers = () => {
               height={337}
               className="w-[150px] drop-shadow-lg xs:w-[200px] sm:w-[300px] md:w-[337px]"
             />
+          </div> */}
+
+          <div className="mx-auto flex h-[110px] w-full items-center justify-center overflow-clip sm:h-[220px]  md:h-[330px] lg:h-[330px]">
+            <Image
+              src={`/images/helpers/research.png`}
+              alt={`Image of a couple doing research`}
+              width={549}
+              height={330}
+              className="mt-4"
+            />
           </div>
-          <div className="flex h-full flex-col justify-around gap-3 ">
-            <Button>
+
+          <div className=" mx-auto flex h-full w-1/2 flex-col items-center justify-around gap-3 sm:mr-4 sm:gap-8 md:gap-16">
+            <Button className="mx-auto w-full">
               <Link href="/recipes">Long COVID Diet</Link>
             </Button>
-            <Button>
+            <Button className="mx-auto w-full">
               <Link href="/news">News and Research</Link>
             </Button>
           </div>
         </div>
       </div>
+      {/* <Breakpoints /> */}
     </div>
   );
 };
--- src/components/HeroSymptoms.tsx
+++ src/components/HeroSymptoms.tsx
@@ -13,6 +13,7 @@ import { EmblaOptionsType } from "embla-carousel";
 // import { SymptomsData } from "../constants/symptomsData";
 import BreakpointIdentifier from "./tgg-utils/BreakpointIdentifier";
 import { SymptomsData } from "@/constants/symptomsData";
+import Breakpoints from "./tgg-utils/Breakpoints";
 
 const Symptoms = () => {
   const OPTIONS: EmblaOptionsType = { loop: true };
@@ -24,6 +25,8 @@ const Symptoms = () => {
       <h2 className="my-8  text-center text-3xl font-medium lg:text-4xl">Our Symptoms</h2>
       <EmblaCarousel slides={SLIDES} options={OPTIONS} symptomsData={SymptomsData} />
       <div className="mt-6"></div>
+      <Breakpoints />
+      <div className="mt-10"></div>
     </section>
   );
 };
