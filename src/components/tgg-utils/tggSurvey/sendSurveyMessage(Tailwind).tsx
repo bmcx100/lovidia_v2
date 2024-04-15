662b7e1996413bafa4bdf413fd602bb8baca992c
Ryan Hurst <bmcx100@gmail.com>
2024-04-15 17:12:39 -0400

    first review fixes v3

--- src/components/tgg-utils/tggSurvey/sendSurveyMessage(Tailwind).tsx
+++ src/components/tgg-utils/tggSurvey/sendSurveyMessage(Tailwind).tsx
@@ -33,19 +33,6 @@ export default function sendSurveyMessage({ formData }: sendSurveyMessageProps) 
               <Text>treatmentsImpact: {formData.treatmentsImpact}</Text>
               <Text>treatmentsHelpful: {formData.treatmentsHelpful}</Text>
               <Text>treatmentComments: {formData.treatmentComments}</Text>
-              <Text className="text-3xl">Impact Info</Text>
-              <Text>impact: {formData.impact}</Text>
-              <Text>affects: {formData.affects}</Text>
-              <Text>managing: {formData.managing}</Text>
-              <Text>impactComments: {formData.impactComments}</Text>
-              <Text className="text-3xl">Support Info</Text>
-              <Text>received: {formData.received}</Text>
-              <Text>additional: {formData.additional}</Text>
-              <Text>supportComments: {formData.supportComments}</Text>
-              <Text className="text-3xl">Suggestions Info</Text>
-              <Text>suggestions: {formData.suggestions}</Text>
-              <Text>topics: {formData.topics}</Text>
-              <Text>feedbackComments: {formData.feedbackComments}</Text>
               <Text className="text-3xl">Consents Info</Text>
               <Text>newsletter: {formData.newsletter}</Text>
               <Text>followup: {formData.followup}</Text>
